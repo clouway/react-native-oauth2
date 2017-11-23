4ef9ecfedc4425c6dd7049c72edd17b293c46884
Martin Milev <martinmariusmilev@gmail.com>
2017-11-23 17:18:30 +0200

    rn-oauth2: initial skeleton for the lib
    Added basic functionality.
    It's a working example how the lib would look like, but fetching credentials
    and other functionalities should be added in future commits.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # react-native-oauth2
 OAuth2 Package for RN
+
+## Usage
+```javascript
+  import Oauth from 'react-native-oauth2'
+
+  const client = new Oauth(APP_ID, APP_CALLBACK, AUTH_URL, TOKEN_URL)
+
+  client.authenticate().then(response => {
+    // authentication was performed 
+  }).catch(error => {
+    // oops, got error during signing in, which could be shown to the user
+  })
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+import { Linking } from 'react-native'
+
+const authorizationUrl = (url, appId, callback, scope, responseType = 'token') =>
+  `${url}?scope=${encodeURIComponent(scope)}&
+  redirect_uri=${encodeURIComponent(callback)}&
+  response_type=${responseType}&
+  client_id=${appId}`.replace(/\s+/g, '')
+
+export default class {
+  constructor(clientId, callback, authUrl, tokenUrl) {
+    this.authenticate = this.authenticate.bind(this)
+    this.clientId = clientId
+    this.callback = callback
+    this.authUrl = authUrl
+    this.tokenUrl = tokenUrl
+  }
+
+  authenticate() {
+    return new Promise((resolve, reject) => {
+      const handleUrl = event => {
+        const authCode = event.url.substring(
+          event.url.indexOf('=') + 1,
+          event.url.length
+        )
+        const tokenRequest = {
+          code: authCode,
+          client_id: this.clientId,
+          redirect_uri: this.callback,
+          grant_type: 'authorization_code'
+        }
+        let s = []
+        for (let key in tokenRequest) {
+          if (tokenRequest.hasOwnProperty(key)) {
+            s.push(`${encodeURIComponent(key)}=${encodeURIComponent(tokenRequest[key])}`)
+          }
+        }
+        const targetUrl = s.join('&')
+        fetch(this.tokenUrl, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: s.join('&')
+        })
+          .then(response => resolve(response))
+          .catch(error => reject(error))
+        Linking.removeEventListener('url', handleUrl)
+      }
+      Linking.addEventListener('url', handleUrl)
+      Linking.openURL(authorizationUrl(this.authUrl, this.clientId, this.callback, 'code'))
+    })
+  }
+}
